ba2a90389522c480f6f07e856f476a28ffc36ea4
Stephen <srchang8@gmail.com>
2022-04-14 12:11:56 -0400

    spring boot with db

---
+++ DocumentDB/src/main/java/com/example/DocumentDB/CryptoStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.DocumentDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoStorageApplication.class, args);
+	}
+
+}
---
+++ DocumentDB/src/test/java/com/example/DocumentDB/CryptoStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocumentDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
